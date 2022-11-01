198ad8998e1b9c7c1c88c923e30c978dd1b06433
Harpreet Singh Chahal <harpreet.chahal1996@gmail.com>
2022-10-31 23:23:28 -0600

    Login and Register Implementation

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,117 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, Button, TextInput } from 'react-native'
 
 
 const LoginScreen = () => {
-    return(
-        <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-            <Text>Login Screen</Text>
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                {/* <Text>Open up App.js to start working on your app!</Text> */}
+                <Text style={styles.mainText}>Create Account</Text>
+                <Image style={styles.logo} source={require('../assets/create_account.png')} />
+            </View>
+            <View style={styles.body}>
+                <Text>Full Name</Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="default"
+                />
+                <Text>Mobile Number</Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="numeric"
+                />
+                <Text>Email</Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="default"
+                />
+                <Text>Password</Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="default"
+                />
+                <Text>Confirm Password</Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="default"
+                />
+                <Text>Agree to terms and conditions</Text>
+           
+           
+                <View style={styles.buttons}>
+                <TouchableOpacity>
+                    <View style={styles.signup}>
+                        <Button color="#3491ff" title="Sign Up" />
+                    </View>
+                    </TouchableOpacity>
+
+                    {/* <StatusBar style="auto" /> */}
+                    <Text>Already have an account?</Text>
+                    <Text style={{ color: '#3491ff' }}>Sign In</Text>
+
+                </View>
+            
+            </View>
         </View>
-    )
+
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        
+        // backgroundColor: '#fff',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    logo: {
+        // marginTop:20,
+        // marginLeft:130,
+        height: 158,
+        width: 120,
+
+    },
+    header: {
+        //backgroundColor: 'skyblue',
+        //flex: 1,
+        flexDirection: 'row',
+        padding: 20,
+
+        marginTop: 25,
+        //height: 300, 
+        //width: '100%',
+        //   alignContent:'space-between',
+        justifyContent: 'space-between',
+    },
+    body: {
+        //flex: 5,
+        padding: 10,
+    },
+    input: {
+        height: 40,
+        //margin: 12,
+        borderWidth: 1,
+        //padding: 10,
+    },
+
+    mainText: {
+        color: 'black',
+        padding: 10,
+        fontSize: 20,
+    },
+    buttons: {
+        marginTop:20,
+        //flex: 2,
+        //width: 100,
+        //padding: 10,
+        // backgroundColor: 'red',
+    },
+    signup: {
+        marginBottom: 10,
+    },
+});
 
 export default LoginScreen
--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,13 +1,114 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import { View, Text, StyleSheet, Image, TouchableOpacity, Button, TextInput } from 'react-native'
 
 
 const RegisterScreen = () => {
-    return(
-        <View style={{flex:1, justifyContent:'center',alignItems:'center'}}>
-            <Text>Register Screen</Text>
+    return (
+
+
+        
+        <View style={styles.container}>
+            <View style={styles.header}>
+                {/* <Text>Open up App.js to start working on your app!</Text> */}
+                <Text style={styles.mainText}>Welcome back</Text>
+                <Image style={styles.logo} source={require('../assets/login.png')} />
+            </View>
+            <View style={styles.body}>
+        
+                <Text>Email</Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="default"
+                />
+                <Text>Password</Text>
+                <TextInput
+                    style={styles.input}
+                    keyboardType="default"
+                />
+                
+                <View style={styles.rememberme}>
+                <Text>Rememeber Me</Text>
+                <Text>Forgot Password</Text>
+                </View>
+           
+           
+                <View style={styles.buttons}>
+                <TouchableOpacity>
+                    <View style={styles.signup}>
+                        <Button color="#3491ff" title="Log In" />
+                    </View>
+                    </TouchableOpacity>
+
+                    {/* <StatusBar style="auto" /> */}
+                    <Text>Don't have an account?</Text>
+                    <Text style={{ color: '#3491ff' }}>Sign Up</Text>
+
+                </View>
+            
+            </View>
         </View>
-    )
+
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        
+        // backgroundColor: '#fff',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    logo: {
+        // marginTop:20,
+        // marginLeft:130,
+        height: 167,
+        width: 179,
+
+    },
+    header: {
+        //backgroundColor: 'skyblue',
+        //flex: 1,
+        flexDirection: 'row',
+        padding: 20,
+
+        marginTop: 25,
+        //height: 300, 
+        //width: '100%',
+        //   alignContent:'space-between',
+        justifyContent: 'space-between',
+    },
+    body: {
+        //flex: 5,
+        padding: 10,
+    },
+    rememberme: {
+        flexDirection:'row',
+        justifyContent:'space-between',
+    },
+    input: {
+        height: 40,
+        //margin: 12,
+        borderWidth: 1,
+        //padding: 10,
+    },
+
+    mainText: {
+        color: 'black',
+        padding: 10,
+        fontSize: 20,
+    },
+    buttons: {
+        marginTop:20,
+        //flex: 2,
+        //width: 100,
+        //padding: 10,
+        // backgroundColor: 'red',
+    },
+    signup: {
+        marginBottom: 10,
+    },
+});
 
 export default RegisterScreen
